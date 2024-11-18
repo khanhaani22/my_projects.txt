7126dee8404015d6cac5f0f055b4cc35817a981e
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-11-18 23:12:15 +0000

    please work ;-;

---
+++ Poker/function.py
@@ -0,0 +1,39 @@
+import random
+import game
+
+def gui_1():
+    print("-=+=-=+=-=+=-=+=-")
+    print("------POKER------")
+    print("-----1.VS AI-----")
+    print("-----2.CLOSE-----")
+    print("-=+=-=+=-=+=-=+=-")
+    
+def start():
+    gui_1()
+    global x
+    x = int(input("-Choose number: "))
+    if x == 1:
+        seq1()
+    if x == 2:
+        exit()
+
+def game_gui():
+    print("-=+=-=+=-=+=-=+=-")
+    print("---Dealt Cards---")
+    print("--{dealt1}-{dealt2}-{dealt3}---{turn}---{river}--")  
+    print("-----------------")
+    print("-AI: {}  {} -")
+    print("-----------------")
+    print("---YOUR  CARDS---")
+    print("-{} {}-")
+    print("-=+=-=+=-=+=-=+=-")
+      
+
+def seq1():
+    createdeck()
+    first_cards()
+    game_gui()
+    print(random.choice(random.choice(deck)))
+    
+    
+start()
--- Poker/game.py
+++ Poker/game.py
@@ -1,11 +1,21 @@
+dealt1 = ""
+dealt2 = ""
+dealt3 = ""
+ai1 = ""
+ai2 = ""
+player1 = ""
+player2 = ""
 
 def createdeck():
     global deck
-    global cards
-    with open(Poker/deck.txt) as file:
-        cards = file.read() 
-    deck =[[],[],[],[]]
-    print (cards)
-    
+    with open("Poker/deck.txt") as file:
+        cards = file.readlines()
+        cards = [line.rstrip('\n') for line in cards]
+    deck = [["D " + item for item in cards],
+            ["H " + item for item in cards],
+            ["C " + item for item in cards],
+            ["S " + item for item in cards]
+            ]
 
-createdeck()
+def first_cards()
+    
